1e4f91df1135c34e132adb66acc95f10b5deceb3
AlbertoCombina <xaratorx@gmail.com>
2023-07-23 11:55:16 +0200

    Non blocking connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
 #include <Arduino.h>
-#include "WiFiMulti.h"
+#include "WiFi.h"
 #include "arduino_secrets.h"
-
-// Creating a WiFi multi instance (This lib is built on top of WiFi.h)
-WiFiMulti wifiMulti;
 
 void setup() {
   Serial.begin(921600);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // Adding an access point, that is your network 
-  // (In theory if you have more then one you can specify SSID and PASS 
-  // for all your network and the board will try to connect to all the networks if the other fails)
-  wifiMulti.addAP(SECRET_SSID, SECRET_PASS);
-
-  // Blocking connection check
-  while(wifiMulti.run() != WL_CONNECTED){
-    delay(100);
-  }
-
-  Serial.print("Connected to ");Serial.println(SECRET_SSID);
-  Serial.print("Local IP address -> ");Serial.println(WiFi.localIP());
+  WiFi.begin(SECRET_SSID,SECRET_PASS);
 }
+
+// Loop variables
+bool isConnected = false;
 
 void loop() {
   // If the WiFi is connected the led is going to switch on
-  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+  if(WiFi.status() == WL_CONNECTED && !isConnected){
+    Serial.print("Connected to ");Serial.println(SECRET_SSID);
+    Serial.print("Local IP address -> ");Serial.println(WiFi.localIP());
+    digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+    isConnected = true;
+  }
 
+
+// If not connected blink the led every second
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(1000);
+    isConnected = false;
+  }
 }
 
