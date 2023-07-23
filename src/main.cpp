d79f9cf7a4411d75b9569e3af6da2f3066c32139
AlbertoCombina <xaratorx@gmail.com>
2023-07-23 12:16:14 +0200

    Blocking with blink

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,24 @@
 #include "WiFi.h"
 #include "arduino_secrets.h"
 
+bool isConnected = false;
+
 void setup() {
   Serial.begin(921600);
   pinMode(LED_BUILTIN, OUTPUT);
 
   WiFi.begin(SECRET_SSID,SECRET_PASS);
+  Serial.print("\nTrying to connect to ");Serial.println(SECRET_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(200);
+  }
+  // Connection succeded
+  isConnected = true;
+  Serial.print("\nCONNECTED!!!");Serial.print("\tLocal IP address -> ");Serial.println(WiFi.localIP());
+  digitalWrite(LED_BUILTIN, LOW);
 }
-
-// Loop variables
-bool isConnected = false;
 
 void loop() {
   // If the WiFi is connected the led is going to switch on
