a88bf19bc91e19a21243e0599746f8bd5f5a77fb
AlbertoCombina <xaratorx@gmail.com>
2023-07-23 11:39:11 +0200

    Successful blocking connection to WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include "WiFiMulti.h"
+#include "arduino_secrets.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Creating a WiFi multi instance (This lib is built on top of WiFi.h)
+WiFiMulti wifiMulti;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(921600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Adding an access point, that is your network 
+  // (In theory if you have more then one you can specify SSID and PASS 
+  // for all your network and the board will try to connect to all the networks if the other fails)
+  wifiMulti.addAP(SECRET_SSID, SECRET_PASS);
+
+  // Blocking connection check
+  while(wifiMulti.run() != WL_CONNECTED){
+    delay(100);
+  }
+
+  Serial.print("Connected to ");Serial.println(SECRET_SSID);
+  Serial.print("Local IP address -> ");Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // If the WiFi is connected the led is going to switch on
+  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
